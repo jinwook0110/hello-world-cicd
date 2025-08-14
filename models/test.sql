82d7c3f86c45aa0e9dde1b4978294c685059f048
jinwook.song@jet-t.co.jp <jinwook.song@jet-t.co.jp>
2025-08-14 08:22:16 +0000

    test.sql

--- models/test.sql
+++ models/test.sql
@@ -1,5 +1,18 @@
-select
-*
-from {{ source('demo', 'bike') }}
+--select
+--*
+----from {{ source('demo', 'bike') }}
 
-limit 10;
+--limit 10;
+
+WITH test_data AS (
+    SELECT 
+        COUNT(*) as row_count,
+        COUNT(DISTINCT RIDE_ID) as unique_rides,
+        MIN(STARTED_AT) as earliest_trip,
+        MAX(STARTED_AT) as latest_trip
+    FROM {{ ref('stg_bike') }}
+)
+
+SELECT 
+    *
+FROM test_data
