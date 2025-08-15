da448cac2a9bedeb51fca99e43f93c01c31cda78
jinwook.song@jet-t.co.jp <jinwook.song@jet-t.co.jp>
2025-08-15 08:06:27 +0000

    jaffle_shop

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,6 @@
+--stg_customers.sql
+select
+    id as customer_id,
+    first_name,
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,7 @@
+--stg_orders.sql
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from {{ source('jaffle_shop', 'orders') }}
